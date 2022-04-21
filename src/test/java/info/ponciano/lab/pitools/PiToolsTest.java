922a2cd27f7837b6495f56d156ee8c92e2eaeaea
Claire Ponciano <prudh.claire@gmail.com>
2022-04-21 13:51:48 +0200

    improve pitools

---
+++ src/test/java/info/ponciano/lab/pitools/PiToolsTest.java
@@ -0,0 +1,48 @@
+package info.ponciano.lab.pitools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PiToolsTest {
+
+    @Test
+    void timeToString() {
+    }
+
+    @Test
+    void dialogOpenFile() {
+    }
+
+    @Test
+    void dialogOpenDir() {
+    }
+
+    @Test
+    void testDialogOpenFile() {
+    }
+
+    @Test
+    void dialogSaveFile() {
+    }
+
+    @Test
+    void readCSV() {
+    }
+
+    @Test
+    void readAllLines() {
+    }
+
+    @Test
+    void readTextFile() {
+    }
+
+    @Test
+    void writeTextFile() {
+    }
+
+    @Test
+    void ls() {
+    }
+}
