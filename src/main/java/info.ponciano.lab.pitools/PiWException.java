92de9064e35deb3219a829c4bf5644297e8af7ac
Claire Ponciano <prudh.claire@gmail.com>
2022-04-21 13:52:12 +0200

    add exception

---
+++ src/main/java/info.ponciano.lab.pitools/PiWException.java
@@ -0,0 +1,7 @@
+package info.ponciano.lab.pitools;
+
+class PiWException extends Exception {
+    public PiWException(String no_workspace_is_defined) {
+        super(no_workspace_is_defined);
+    }
+}
