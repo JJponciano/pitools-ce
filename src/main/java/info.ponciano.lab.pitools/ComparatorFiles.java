697498b857ec2dbabd58b85d0070e8ee08981fd0
Claire Ponciano <prudh.claire@gmail.com>
2022-04-27 15:12:01 +0200

    Add comparator Files

--- src/main/java/info.ponciano.lab.pitools/ComparatorFiles.java
+++ src/main/java/info.ponciano.lab.pitools/ComparatorFiles.java
@@ -1,7 +1,9 @@
 package info.ponciano.lab.pitools;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,13 +12,14 @@ import java.util.List;
  */
 public class ComparatorFiles {
 
-    /** Compare the content of two files (one line to all others) and return common content
+    /** Compare the content of two files (one line to all others) and return common or different content
      * @param path1 path of the first file to compare
      * @param path2 path of the second file to compare
+     * @param getCommon true to get only common content, false to get only the different content.
      * @param nbcharacters number of first character use to create the prefix for the comparison for each line (0 for complet matching)
-     * @return list of the prefixes that are common to the second file
+     * @return list of the prefixes that are common or different to the second file
      */
-    public static List<String> containsLine(String path1, String path2, int nbcharacters) throws IOException {
+    public static List<String> compareLines(String path1, String path2, int nbcharacters,boolean getCommon) throws IOException {
         List<String> common=new ArrayList<>();
         // read files
         List<String> l1 = PiTools.readAllLines(path1);
@@ -25,7 +28,7 @@ public class ComparatorFiles {
         for (String l:l1) {
             String prefix;
             //get the prefix
-            if(nbcharacters>0)
+            if(nbcharacters>0&&nbcharacters<l.length())
                 prefix=l.substring(0,nbcharacters);
             else
                 prefix=l;
@@ -37,17 +40,20 @@ public class ComparatorFiles {
                     match=true;
                 else i++;
             }
+            //if it is different, inverse the matching
+            if(!getCommon)match=!match;
             if(match&&!common.contains(prefix))
                 common.add(prefix);
         }
         return  common;
     }
 
+
     public static void main(String[] args) {
         String f1 = PiTools.dialogOpenFile(null, new FileNameExtensionFilter("txt","txt"), "");
         String f2 = PiTools.dialogOpenFile(null, new FileNameExtensionFilter("txt","txt"), f1);
         try {
-            List<String> common = ComparatorFiles.containsLine(f1, f2, 14);
+            List<String> common = ComparatorFiles.compareLines(f1, f2, 14,false);
             String s = PiTools.dialogSaveFile(null, new FileNameExtensionFilter("txt","txt"),f2);
             StringBuilder results= new StringBuilder(common.size() + "\n");
             for (String l : common) {
@@ -58,6 +64,26 @@ public class ComparatorFiles {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static void copyFilesStartWith(String startwithfile, String dir, String dest) throws IOException {
+        List<String> startw = PiTools.readAllLines(startwithfile);
+        List<String> files = PiTools.ls(new File(dir), "*", true);
 
+        for (String f : files) {
+            boolean startwith=false;
+            int i=0;
+            while (!startwith&&i<startw.size()){
+                if(f.startsWith(startw.get(i)))startwith=true;
+                else i++;
+            }
+            String input = dir + f;
+            if(startwith){
+                String df = dest + f;
+                Files.copy(new File(input).toPath(),new File(df).toPath());
+                System.out.println(input+"->"+df);
+            }else
+            System.out.println(input+"-> X");
+
+        }
     }
 }
