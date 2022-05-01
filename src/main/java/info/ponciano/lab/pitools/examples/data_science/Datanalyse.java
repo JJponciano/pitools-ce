073ea234711023f3cfad2fc2dbaedf0c3b2620f8
Claire Ponciano <prudh.claire@gmail.com>
2022-05-01 17:41:50 +0200

    Add an example of data results analyse

---
+++ src/main/java/info/ponciano/lab/pitools/examples/data_science/Datanalyse.java
@@ -0,0 +1,64 @@
+package info.ponciano.lab.pitools.examples.data_science;
+
+import info.ponciano.lab.pitools.PiTools;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class helps in data science analysis based on CSV file.
+ */
+public class Datanalyse {
+    private final List<Category> categories;
+
+    public Datanalyse() {
+        this.categories = new ArrayList<>();
+    }
+    public void save(String dir,String... excepted){
+        File directory=new File(dir);
+        if(directory.exists()) PiTools.deleteDirectory(directory);
+        boolean mkdir = directory.mkdir();
+        this.categories.forEach(c->PiTools.writeTextFile(directory+"/"+c.getName()+".csv",c.toString()));
+        Category summary = Category.merge("Results",this.categories,"size");
+        summary.sort();
+        summary.calculateMetrics(7,excepted);
+        PiTools.writeTextFile(directory+"/"+summary.getName()+".csv",summary.toString());
+    }
+    public void loadCSV(String directory, boolean recursively,String... excepted) throws IOException {
+        List<File> csvs = PiTools.lsFiles(new File(directory), "csv", recursively);
+        //read each csv file
+        for (File csv : csvs) {
+            String[][] data = PiTools.readCSV(csv.getPath(), ",");
+            // fist value is the category, second value the id reference, third the id for the comparison, and fourth the comparison value
+            for (String[] row : data) {
+                Category cat = new Category(row[0]);
+                if (!categories.contains(cat)) categories.add(cat);
+                else cat = this.categories.get(this.categories.indexOf(cat));
+                String refid = row[1];
+                for (int i = 2; i < row.length - 1; i++) {
+                    String id = row[i];
+                    i++;
+                    double value = Double.parseDouble(row[i]);
+                    cat.add(refid, id, value);
+                }
+            }
+        }
+        this.categories.forEach(c->{c.sort();c.calculateMetrics(7,excepted);});
+    }
+
+    public static void main(String[] args) {
+        String dir="C:\\Users\\49151\\Documents\\data\\best_results";
+        String res="C:\\Users\\49151\\Documents\\data\\best_results_analyse";
+        String []excepted={"1347382-000-01" ,"1355565-000-00","1395819-000-00","1531788-003-00","1592582-000-00","1592582-000-01"};
+        Datanalyse csv=new Datanalyse();
+        try {
+            csv.loadCSV(dir, false,excepted);
+            csv.save(res,excepted);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
