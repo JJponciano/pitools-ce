cc7b632b248d544c8c872cbf64b7e50f22cbb0f2
Claire Ponciano <prudh.claire@gmail.com>
2022-05-01 11:13:16 +0200

    Test lsPath

--- src/main/java/info.ponciano.lab.pitools/PiTools.java
+++ src/main/java/info.ponciano.lab.pitools/PiTools.java
@@ -383,5 +383,27 @@ public class PiTools {
         return result.stream().sorted().toList();
     }
 
+    public static void deleteDirectory(File directory) {
 
+        // if the file is directory or not
+        if(directory.isDirectory()) {
+            File[] files = directory.listFiles();
+
+            // if the directory contains any file
+            if(files != null) {
+                for(File file : files) {
+
+                    // recursive call if the subdirectory is non-empty
+                    deleteDirectory(file);
+                }
+            }
+        }
+
+        if(directory.delete()) {
+            System.out.println(directory + " is deleted");
+        }
+        else {
+            System.out.println("Directory not deleted");
+        }
+    }
 }
--- src/test/java/info/ponciano/lab/pitools/PiToolsTest.java
+++ src/test/java/info/ponciano/lab/pitools/PiToolsTest.java
@@ -65,7 +65,30 @@ class PiToolsTest {
         assertEquals("t1.txt", results.get(0));
         assertEquals("t2.txt", results.get(1));
         assertEquals("t3.txt", results.get(2));
+    }
+    @Test
+    void lsPath() {
+        File dirTest=new File("test_pitools");
+        File dirTest2=new File(dirTest.getPath()+"/zsub_test_pitools");
+        dirTest.mkdir();
+        dirTest2.mkdir();
+        PiTools.writeTextFile(dirTest.getPath()+"/t1.txt","file 1\n for testing");
+        PiTools.writeTextFile(dirTest.getPath()+"/t2.txt","file 2\n for testing");
+        PiTools.writeTextFile(dirTest.getPath()+"/t2.xyz","file 3\n for testing");
+        PiTools.writeTextFile(dirTest2.getPath()+"/t3.txt","file 4\n for testing");
+        PiTools.writeTextFile(dirTest2.getPath()+"/t3.xyz","file 5\n for testing");
 
+        List<String> results = PiTools.lsPath(dirTest, "txt", true);
+        List<String> resultsNR = PiTools.lsPath(dirTest, "txt", false);
+        assertEquals(3, results.size());
+        assertEquals(2, resultsNR.size());
+
+        assertTrue((dirTest.getPath()+"/t1.txt").equals(results.get(0))||(dirTest.getPath()+"\\t1.txt").equals(results.get(0)));
+        assertTrue((dirTest.getPath()+"/t2.txt").equals(results.get(0))||(dirTest.getPath()+"\\t2.txt").equals(results.get(1)));
+        assertTrue((dirTest2.getPath()+"/t3.txt").equals(results.get(0))||(dirTest2.getPath()+"\\t3.txt").equals(results.get(2)));
+
+
+        PiTools.deleteDirectory(dirTest);
     }
 
     @Test
